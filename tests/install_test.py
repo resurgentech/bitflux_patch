a5fc810651f79b1f5744056d264898a1379c145b
Jared Hulbert <jaredeh@gmail.com>
2022-03-22 22:49:02 -0700

    better testing diagnositics

--- tests/install_test.py
+++ tests/install_test.py
@@ -161,7 +161,7 @@ def run_tests(configs, args, loops):
     print("++++++++++++++++PASSED COLLECTOR CHECK++++++++++++++++++++++++++++", flush=True)
 
     memhog(configs, args)
-    sleep(10)
+    sleep(300)
 
     if check_for_memhog(configs, args):
         print("----------------FAILED MEMHOG CHECK-----------------------------", flush=True)
