d8aa72b5129c0dd8cb80b1104b560c03bd522d2a
Jared Hulbert <jaredeh@gmail.com>
2025-01-27 23:53:41 -0800

    fixing fs/proc/* splice

--- scripts/kernel_package_builder/deb.py
+++ scripts/kernel_package_builder/deb.py
@@ -237,21 +237,27 @@ def deb_set_flavour(flavour, orig_flavour, debian_dir, allow_errors=False, verbo
     for a in file_pair:
         duplicate_file(a[0], a[1], workingdir=debian_dir, verbose=verbose)
         if a[1] == f"control.d/{flavour}.inclusion-list":
+            spliced = False
             # we need to add fs/proc/ to the inclusion list for the swaphints.ko to be added to the modules deb
             with open(os.path.join(debian_dir, a[1]), 'r') as file:
                 lines = file.readlines()
+            with open(os.path.join(debian_dir, f"{a[1]}.bak"), 'w') as file:
+                for line in lines:
+                    file.write(line)
             newlines = []
             for i in range(len(lines)):
-                line = lines[i]
-                if line.startswith('fs/'):
+                line = lines[i].strip()
+                if (not spliced) and line.startswith('fs/'):
                     b = line.split('/')
+                    print(f"b={b} {b[1][0] > 'proc'}")
                     if b[1][0] > 'proc':
                         newlines.append('fs/proc/*')
-                        continue
+                        spliced = True
                 newlines.append(line)
             if not 'fs/proc/*' in newlines:
                 raise BaseException("Missing 'fs/proc/*' in {}".format(a[1]))
             with open(os.path.join(debian_dir, a[1]), 'w') as file:
+                newlines.append('')
                 file.write('\n'.join(newlines))
     sed_sets = [
         # We might not need this either...
@@ -615,6 +621,7 @@ def debian_style_build(distro_config_path, buildnumber, maintainer, verbose, nob
     # Handle flavour hacking
     printfancy("Creating flavour swaphints config files")
     deb_set_flavour(flavour, orig_flavour, debian_dir, verbose=True)
+    raise
     commit_and_create_patch('flavour', src_dir, verbose=True)
 
     # Create the final patching
